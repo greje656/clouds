1977b807260518f490fd8e1dbca03b353fcf9922
Jp <jean-philippe.grenier@autodesk.com>
2016-10-17 13:53:59 -0400

    Adding proper namespace to access renderer.

--- clouds-resources/clouds.lua
+++ clouds-resources/clouds.lua
@@ -2,9 +2,9 @@
 Clouds = {}
 
 function Clouds:generate()
-	cloud_resource1 = Renderer.resource("clouds_result_texture1")
-	cloud_resource2 = Renderer.resource("clouds_result_texture2")
-	cloud_resource3 = Renderer.resource("clouds_result_texture3")
+	cloud_resource1 = stingray.Renderer.resource("clouds_result_texture1")
+	cloud_resource2 = stingray.Renderer.resource("clouds_result_texture2")
+	cloud_resource3 = stingray.Renderer.resource("clouds_result_texture3")
 
 	resource_maping1 = {}
 	resource_maping2 = {}
@@ -18,9 +18,9 @@ function Clouds:generate()
 	resource_maping2["output"] = cloud_resource2
 	resource_maping3["output"] = cloud_resource3
 
-	Renderer.run_resource_generator("cloud_generator_texture1", resource_maping1)
-	Renderer.run_resource_generator("cloud_generator_texture2", resource_maping2)
-	Renderer.run_resource_generator("cloud_generator_texture3", resource_maping3)
+	stingray.Renderer.run_resource_generator("cloud_generator_texture1", resource_maping1)
+	stingray.Renderer.run_resource_generator("cloud_generator_texture2", resource_maping2)
+	stingray.Renderer.run_resource_generator("cloud_generator_texture3", resource_maping3)
 end
 
 function Clouds.spawned(world, instances)
